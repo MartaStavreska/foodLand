7dd8268130fb0849a74ecde6ffe0d21fc8aad1c3
MartaStavreska <stavreskamarta@gmail.com>
2020-05-03 16:31:28 +0200

    All recepie

--- js/all-recepies.js
+++ js/all-recepies.js
@@ -3,14 +3,13 @@ var recepies;
 allRecepies();
 
 function allRecepies() {
-   
+
     var url = "https://food-blog-8ca4b.firebaseio.com/recepies.json";
 
     fetch(url)
         .then(function (res) {
             return res.json();
         }).then(function (data) {
-            
             recepies = Object.keys(data).map(function (element) {
                 var newElement = {
                     id: element,
@@ -23,16 +22,36 @@ function allRecepies() {
                 return newElement;
 
             });
+
             drawAllRecepies(recepies);
+            searchRecepie(recepies);
+
         }).catch(function (error) {
             console.log(error)
         });
 }
 
 
-function drawAllRecepies(array) { 
+function searchRecepie(arrayOfRecepies) {
+    var input = document.querySelector("#search").value;
+    var formated = input.toLowerCase().trim();
+
+    var filtered = arrayOfRecepies.filter(function (element) {
+        return element.name.toLowerCase().indexOf(formated) > -1 ||
+            element.recepie.toLowerCase().indexOf(formated) > -1
+
+    });
+
+    console.log(filtered)
+    drawAllRecepies(filtered);
+}
+
+
+
+
+
+function drawAllRecepies(array) {
     array.forEach(function (element) {
-        
         var rec = `
         <div class="card mb-5">
         <div class="row no-gutters">
@@ -41,15 +60,15 @@ function drawAllRecepies(array) {
             <p class="card-title recepie">Recepie by: ${element.name}</p>
             <img src="${element.image}" class="card-img mt-3"
                     alt="...">
-            
+
             </div>
             <div class="col-md-8 mt-4">
                 <div class="card-body text-justify">
                       <div class="d-flex">
 
-                        <div class="p-3 col-md-4" >
+                        <div class="p-3 col-md-5" >
                             <h5>Ingredient</h5>
-                            <p class="card-text">${element.ingredients}</p>
+                            <p class="card-text"><pre>${element.ingredients}</pre></p>
                         </div>
                         <div class="p-3">
                             <h5>Preparation</h5>
@@ -61,13 +80,11 @@ function drawAllRecepies(array) {
             </div>
         </div>
     </div>`
-        document.querySelector(".container").innerHTML += rec;
-       
-       
-    });
-      
-}
 
+
+        document.querySelector(".container").innerHTML += rec;
+    });
+}
 
 
 
--- js/recepie.js
+++ js/recepie.js
@@ -16,25 +16,28 @@ fetch(url).then(function (res) {
 function drawEachRecepie(data) {
     var recepie =
         `
-        <div class="mt-3">
-        <h3 class="pt-3 offset-2">${data.recepie}</h3>
-        <p id="name" class="pt-3 offset-3 ">Recepie by: ${data.name}</p>
-        <div class="d-flex  text-justify">
-            <div class="col-md-3 ml-3 mt-4">
-                <h5>Ingredients</h5>
-                <p>${data.ingredients}</p>
+  
+        <div class="mt-3 col-md-12 text-center border-top border-bottom">
+          <h3 class="pt-3">${data.recepie}</h3>
+          <p id="name" class="pt-3  ">Recepie by: ${data.name}</p>
+          <div class="d-flex  text-justify border-top border-bottom">
+            <div class="col-md-3 mt-4">
+              <h5>Ingredients</h5>
+              <p>
+                <pre>${data.ingredients}<pre></p>
             </div>
-            <div class="col-md-6 offset-1 mt-4">
+            <div class="col-md-6  mt-4">
                 <h5>Preparation</h5>
                 <p>${data.preparation}</p>
             </div>
-        </div>
-
-        </div>
-
-        <div class="col-md-5 mt-3 text-center">
-        <img src="${data.image}" class="w-100" alt="">
-        </div> 
-  `
+            <div class="col-md-3 p-2 text-center ml-3">
+              <img src="${data.image}" class="w-100" style="height:400px;" alt="">
+            </div> 
+          </div>
+      
+       
+       
+          `
     document.querySelector(".container-fluid").innerHTML += recepie;
 }
+ 
