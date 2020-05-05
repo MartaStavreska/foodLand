cc98e355c50166881a503b175cbe52aa1fe80f77
MartaStavreska <stavreskamarta@gmail.com>
2020-05-05 20:31:11 +0200

    Promena

--- js/recepie.js
+++ js/recepie.js
@@ -16,24 +16,25 @@ fetch(url).then(function (res) {
 function drawEachRecepie(data) {
     var recepie =
         `
-  
-        <div class="mt-3 col-md-12 text-center border-top border-bottom">
-          <h3 class="pt-3">${data.recepie}</h3>
-          <p id="name" class="pt-3  ">Recepie by: ${data.name}</p>
-          <div class="d-flex  text-justify border-top border-bottom">
-            <div class="col-md-3 mt-4">
+        <div class="mt-3 col-md-12 text-center border-bottom border-top">
+        <h3 class="pt-3">${data.recepie}</h3>
+        <p id="name" class="pt-3 border-bottom">Recepie by: ${data.name}</p>
+        <div class="text-justify">
+          <div class="d-flex offset-2">
+          <div class="col-md-5 mt-4">
               <h5>Ingredients</h5>
-              <p>
-                <pre>${data.ingredients}<pre></p>
+              <p><pre>${data.ingredients}<pre></p>
             </div>
-            <div class="col-md-6  mt-4">
-                <h5>Preparation</h5>
-                <p>${data.preparation}</p>
-            </div>
-            <div class="col-md-3 p-2 text-center ml-3">
-              <img src="${data.image}" class="w-100" style="height:400px;" alt="">
-            </div> 
+             <div class="col-md-4">
+               <img src="${data.image}" class="w-100" style="max-height:400px"  alt="">
+           </div>
           </div>
+           <div class="col-md-8 offset-2">
+              <h5>Preparation</h5>
+              <p class="mt-3">${data.preparation}</p>
+          </div> 
+        </div>
+        </div>
       
        
        
--- js/set-header-and-footer.js
+++ js/set-header-and-footer.js
@@ -3,13 +3,19 @@ function setHeaderAndFooter() {
     document.querySelector("header").innerHTML += `
     <nav class="navbar navbar-expand-md navbar-light bg-light">
     <a href="home.html" class="navbar-brand"><img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSkCw6W0YMvcNzS0JSt9TnhksR2EXWf9WGF4Rex57a6ffhREfaT&usqp=CAU"
+        src="images/400-4002708_chef-hat-clip-art-chef-hat-svg-free.jpg"
         width="30" height="30" class="d-inline-block align-top" alt=""></a>
     <button type="button" class="navbar-toggler bg-light" data-toggle="collapse" data-target="#nav">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-between" id="nav">
-      <ul class="navbar-nav">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link  px-3" href="home.html">Home<span class="sr-only"></span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  px-3" href="events.html">Events<span class="sr-only"></span></a>
+        </li>
         <li class="nav-item">
           <a class="nav-link  px-3" onclick="AddRecepie()">Add Recepie<span class="sr-only"></span></a>
         </li>
@@ -23,15 +29,7 @@ function setHeaderAndFooter() {
           <a class="nav-link  px-3" href="events.html">Events<span class="sr-only"></span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  px-3" href="contact.html">Contact<span class="sr-only"></span></a>
-        </li>
-        <li class="nav-item">
-          <a id="admin" class="nav-link  px-3" href="messages.html" onclick="getMessages()"><span
-              class="sr-only"></span></a>
-        </li>
-        <li class="nav-item">
-          <a id="logout" class="nav-link  px-3" href="home.html" onclick="LogOut()"><span
-              class="sr-only"></span></a>
+          <a id="logout" class="nav-link  px-3" href="home.html" onclick="LogOut()"><span class="sr-only"></span></a>
         </li>
       </ul>
     </div>
